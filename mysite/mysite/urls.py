c9a83b3babd6a08e655a4b2ed954e4c2860e7c40
Jumpei Yoshimura <junpei.yoshimura@iridge.jp>
2018-11-16 20:41:04 +0900

    Add tutorial

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.conf.urls import include, url, re_path
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from search import views as search_views
+
+urlpatterns = [
+    url(r'^django-admin/', admin.site.urls),
+
+    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+]
+
+urlpatterns += i18n_patterns(
+    # These URLs will have /<language_code>/ appended to the beginning
+
+    re_path(r'^search/$', search_views.search, name='search'),
+
+    re_path(r'', include(wagtail_urls)),
+)
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
